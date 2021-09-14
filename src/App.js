d3cdc5a21661cdabba2755774f0307f658efc022
liliput35 <83822871+liliput35@users.noreply.github.com>
2021-09-14 13:08:48 +0800

    deploy my react app to github pages

---
+++ src/App.js
@@ -0,0 +1,102 @@
+import { useState, useEffect } from "react"
+import React from 'react';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Tasks from './components/Tasks'
+import AddForm from './components/AddForm'
+
+
+function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
+
+  useEffect( () => {
+    const getTasks = async () => {
+      const taskFromServer = await fetchTask()
+      setTasks(taskFromServer)
+    }
+
+    getTasks()
+  }, [])
+
+  // fetch tasks
+  const fetchTask = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
+  // fetch task
+  const fetch1Task = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+
+    return data
+  }
+
+  //delete task
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, 
+    {
+      method: 'DELETE'
+    })
+
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  //toggle reminder
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetch1Task (id)
+    const updTask = {...taskToToggle, 
+    reminder: !taskToToggle.reminder }
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`,{ 
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/json'
+    }, 
+    body: JSON.stringify(updTask)
+  })
+
+    const data = await res.json()
+
+    setTasks(
+      tasks.map((task) =>
+       task.id === id ? 
+    {...task, reminder: data.reminder} : task))
+  }
+
+  //addTask
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST', 
+      headers: {
+        'Content-type': 'application/json'
+      }, 
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+
+    //const id = Math.floor(Math.random() * 10000) + 1;
+
+    //const newTask = { id, ...task }
+    //setTasks([...tasks, newTask])
+  }
+
+  return (
+    <div className="container">
+     <Header onAdd={ () => setShowAddTask(!showAddTask)}
+     showAdd={showAddTask}/>
+     {showAddTask && <AddForm onAdd={addTask}/>}
+     {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+      : 'No Tasks To Show'}
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Copyright &copy; 2021</p>
+            <p>Built by Lorenz Ciocon</p>
+        </footer>
+    )
+}
+
+export default Footer
